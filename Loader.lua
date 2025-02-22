b0f56269453590c8ec45ab7623880a09a10b75a4
iza308 <149947867+iza308@users.noreply.github.com>
2025-02-23 00:05:07 +0700

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,29 @@
+setclipboard("https://discord.gg/FJFZMCu5JX")
+wait(1)
+local repo = "https://raw.githubusercontent.com/iza308/Zeld-Hub/refs/heads/main/Module/Games/"
+local function LoadScript(ScriptName)
+	pcall(function()
+		t = 0
+		repeat
+			local s, r = pcall(function()
+				loadstring(game:HttpGet(repo .. ScriptName))()
+			end)
+			if not s then
+				spawn(function()
+					error(r)
+				end)
+			end
+			t = t + 1
+			wait(1)
+		until getgenv().Executed or t >= 1
+	end)
+end
+local Id = game.PlaceId
+local GameId = game.GameId
+local PlaceIds = {
+	["TRSE"] = { 70876832253163, 116495829188952 },
+}
+if table.find(PlaceIds["DR"], GameId) then
+	LoadScript("Dead%20Rails.lua")
+end
+
