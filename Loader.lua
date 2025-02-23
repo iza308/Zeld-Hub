65acfcdeec3492c962dd0f6049679d5aaadbdbfc
iza308 <149947867+iza308@users.noreply.github.com>
2025-02-23 08:46:04 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,6 +1,6 @@
 setclipboard("https://discord.gg/FJFZMCu5JX")
 wait(1)
-local repo = "https://raw.githubusercontent.com/iza308/Zeld-Hub/refs/heads/main/Module/Games/"
+local repo = "https://raw.githubusercontent.com/iza308/Zeld-Hub/main/Module/Games/"
 local function LoadScript(ScriptName)
 	pcall(function()
 		t = 0
@@ -21,9 +21,29 @@ end
 local Id = game.PlaceId
 local GameId = game.GameId
 local PlaceIds = {
+	["RG"] = { 914010731 },
+	["PTMEBGE"] = { 3411100258 },
 	["DR"] = { 70876832253163, 116495829188952 },
+	["Chaos"] = { 6441847031 },
+    ["BaldeBall"] = { 14732610803, 13772394625 },
+    ["FTZ"] = { 4954096313, 5096191125, 5941839954 },
+    ["RT2"] = { 6299805723 },
+    ["TB2"] = { 7305309231 },
 }
 if table.find(PlaceIds["DR"], GameId) then
 	LoadScript("Dead%20Rails.lua")
+elseif table.find(PlaceIds["RG"], Id) then 
+	LoadScript("RoGhoul.lua")
+elseif table.find(PlaceIds["BaldeBall"], Id) then 
+	LoadScript("Blade%20Ball.lua")
+elseif table.find(PlaceIds["Chaos"], Id) then 
+	LoadScript("Chaos.lua")
+elseif table.find(PlaceIds["FTZ"], Id) then 
+	LoadScript("Field%20Trip%20Z.lua")
+elseif table.find(PlaceIds["PTMEBGE"], Id) then 
+	LoadScript("prtty%20much%20evry%20bordr%20gam%20evr.lua")
+elseif table.find(PlaceIds["RT2"], Id) then 
+	LoadScript("Restaurant%20Tycoon%202.lua")
+elseif table.find(PlaceIds["TB2"], Id) then 
+	LoadScript("Taxi%20Boss.lua")
 end
-
